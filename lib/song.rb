6e5b3717a9ecf518a55f63b8639718335bd24dec
Joran <jordanalireed@icloud.com>
2020-11-12 19:19:50 -0500

    commit tree 6a87447ddd07966608293206b252ab62a62286e2

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+arr = [1,2,3,4,5]
+
+song = {
+  # Key     # value
+  "Numbeer 1" => 1,
+  :number2 => 2
+}
+
+
+arr.first
+arr[2]
+
+puts song["Number 1"]
+
+song["Number 3"] = 3
+
+song["Number 1"] = 5
